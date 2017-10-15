69dc67a36c851b20345463f2681a9e8da1e188e5
jungwook <jwcris.park@gmail.com>
2017-10-15 14:01:55 +0000

    update smartcab project 1st submit

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -24,7 +24,6 @@ class LearningAgent(Agent):
         ###########
         # Set any additional class parameters as needed
 
-
     def reset(self, destination=None, testing=False):
         """ The reset function is called at the beginning of each trial.
             'testing' is set to True if testing trials are being used
@@ -39,7 +38,15 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
-
+        
+        # Q-Learning Simulation Results : Start
+        if testing:
+            self.epsilon = 0
+            self.alpha = 0
+        else:
+            self.epsilon = self.epsilon - 0.05
+         # Q-Learning Simulation Results : End
+           
         return None
 
     def build_state(self):
@@ -55,13 +62,20 @@ class LearningAgent(Agent):
         ########### 
         ## TO DO ##
         ###########
+        
+        # NOTE : you are not allowed to engineer eatures outside of the inputs available.
+        # Because the aim of this project is to teach Reinforcement Learning, we have placed 
+        # constraints in order for you to learn how to adjust epsilon and alpha, and thus learn about the balance between exploration and exploitation.
+        # With the hand-engineered features, this learning process gets entirely negated.
+        
         # Set 'state' as a tuple of relevant data for the agent        
-        state = None
-
+        state = (waypoint, inputs['light'], inputs['oncoming'], inputs['left']) # Q-Learning Simulation Results
+        self.createQ(state)
+        
         return state
 
 
-    def get_maxQ(self, state):
+    def get_maxQ(self):
         """ The get_max_Q function is called when the agent is asked to find the
             maximum Q-value of all actions based on the 'state' the smartcab is in. """
 
@@ -70,9 +84,22 @@ class LearningAgent(Agent):
         ###########
         # Calculate the maximum Q-value of all actions for a given state
 
-        maxQ = None
+        maxQ = 0
+        actions = []
 
-        return maxQ 
+        for action in self.Q[self.state]:
+            
+            Qvalue = self.Q[self.state][action]
+            
+            if maxQ < Qvalue:
+                maxQ = Qvalue
+                del actions[:]
+                actions.append(action)
+                
+            elif maxQ == Qvalue:
+                actions.append(action)
+        
+        return maxQ, actions 
 
 
     def createQ(self, state):
@@ -85,6 +112,9 @@ class LearningAgent(Agent):
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
 
+        if self.learning and state not in self.Q.keys():
+            self.Q[state] = {'left':0.0, 'right':0.0, 'forward':0.0, None:0.0}
+        
         return
 
 
@@ -95,21 +125,29 @@ class LearningAgent(Agent):
         # Set the agent state and default action
         self.state = state
         self.next_waypoint = self.planner.next_waypoint()
-        action = None
+        #action = random.choice(self.valid_actions) # 4.1 Basic Agent Simulation Results
 
         ########### 
         ## TO DO ##
         ###########
         # When not learning, choose a random action
         # When learning, choose a random action with 'epsilon' probability
-        #   Otherwise, choose an action with the highest Q-value for the current state
- 
+        # Otherwise, choose an action with the highest Q-value for the current state
+        # Be sure that when choosing an action with highest Q-value that you randomly select between actions that "tie".
+        if self.learning:
+            if self.epsilon > random.random():
+                action = random.choice(self.valid_actions)
+            else:
+                action = random.choice((self.get_maxQ())[1])               
+        else:
+            action = random.choice(self.valid_actions)
+
         return action
 
 
     def learn(self, state, action, reward):
         """ The learn function is called after the agent completes an action and
-            receives an award. This function does not consider future rewards 
+            receives a reward. This function does not consider future rewards 
             when conducting learning. """
 
         ########### 
@@ -117,6 +155,8 @@ class LearningAgent(Agent):
         ###########
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
+        if self.learning:
+            self.Q[self.state][action] = (1 - self.alpha) * self.Q[self.state][action] + self.alpha * reward
 
         return
 
@@ -153,13 +193,13 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent)
+    agent = env.create_agent(LearningAgent, learning=True, epsilon=5, alpha=0.1)
     
     ##############
     # Follow the driving agent
     # Flags:
     #   enforce_deadline - set to True to enforce a deadline metric
-    env.set_primary_agent(agent)
+    env.set_primary_agent(agent, enforce_deadline=True) # 4.1 Basic Agent Simulation Results
 
     ##############
     # Create the simulation
@@ -168,14 +208,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env)
+    sim = Simulator(env, update_delay=0.1, log_metrics=True, optimized=True) # 4.1 Basic Agent Simulation Results
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run()
+    sim.run(n_test=20, tolerance=0.02) # 4.1 Basic Agent Simulation Results
 
 
 if __name__ == '__main__':
